6d286e807f5fe8b9cffd93a25164080a4dfba8d0
= <91413713+Nikoo-Asadnejad@users.noreply.github.com>
2022-06-13 22:11:54 +0430

    Feat : add Configurator class

---
+++ RedisHelperDll/RedisHelperDll/Configurations/RedisHelperConfigurator.cs
@@ -0,0 +1,19 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisHelperDll.Configurations
+{
+  public static class RedisHelperConfigurator
+  {
+    public static void InjectServices(IServiceCollection services, IConfiguration configuration)
+    {
+      services.AddStackExchangeRedisCache(options =>
+      options.Configuration = configuration["RedisConnection"]);
+    }
+  }
+}
